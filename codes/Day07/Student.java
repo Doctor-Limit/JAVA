8560221a12dc6e98db6a3d46e7f759e2646d0373
Doctor-Limt <mr.chen.150311@foxmail.com>
2022-03-21 16:13:46 +0800

    test6

--- codes/Day07/Student.java
+++ codes/Day07/Student.java
@@ -10,7 +10,8 @@ public class Student extends Person{
     public void study(){
         System.out.println("学习专业是："+major);
     }
-    public void eat(){
+    public void eat(){ //子类中叫重写方法，父类叫被重写方法
         System.out.println("学生应该多吃有营养的食物");
     }
+
 }
