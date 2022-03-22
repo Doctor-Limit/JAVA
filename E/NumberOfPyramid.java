9789c6d4c87fc019dc31d5a0f6d608cafcd8d773
Doctor-Limt <mr.chen.150311@foxmail.com>
2022-03-22 14:24:57 +0800

    test6

---
+++ E/NumberOfPyramid.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class NumberOfPyramid {
+    public static void main(String args[]){
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        int i,j;
+        for(i=1;i<=n;i++){
+            for(j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            int temp=i;
+            while(temp!=0){
+                System.out.print(temp--);
+            }
+            for(int x=temp+2;x<=i;x++){
+                System.out.print(x);
+            }
+            System.out.println();
+        }
+    }
+}
--- codes/Day08/CheckTest.java
+++ codes/Day08/CheckTest.java
@@ -2,15 +2,18 @@ package Day08;
 
 public class CheckTest {
     public static void main(String[] args) {
-        CheckAccount acct=new CheckAccount(1122,20000,0.045,5000);
-        acct.withdraw(5000);
-        System.out.println("您的账户余额为："+acct.getBalance());
-        System.out.println("您的可透支额度为："+acct.getOverdraft());
-        acct.withdraw(18000);
-        System.out.println("您的账户余额为："+acct.getBalance());
-        System.out.println("您的可透支额度为："+acct.getOverdraft());
-        acct.withdraw(3000);
-        System.out.println("您的账户余额为："+acct.getBalance());
-        System.out.println("您的可透支额度为："+acct.getOverdraft());
+//        CheckAccount acct=new CheckAccount(1122,20000,0.045,5000);
+//        acct.withdraw(5000);
+//        System.out.println("您的账户余额为："+acct.getBalance());
+//        System.out.println("您的可透支额度为："+acct.getOverdraft());
+//        acct.withdraw(18000);
+//        System.out.println("您的账户余额为："+acct.getBalance());
+//        System.out.println("您的可透支额度为："+acct.getOverdraft());
+//        acct.withdraw(3000);
+//        System.out.println("您的账户余额为："+acct.getBalance());
+//        System.out.println("您的可透支额度为："+acct.getOverdraft());
+        int i=0;
+        i=i++;
+        System.out.println(i);
     }
 }
